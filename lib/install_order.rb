2fb7737dc4c079f8e6390f0eb4127602a63de4bd
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-01-09 10:35:59 -0800

    Completes install order

--- lib/install_order.rb
+++ lib/install_order.rb
@@ -8,7 +8,35 @@
 # Import any files you need to
 
 require_relative 'graph'
+require_relative 'topological_sort'
+require 'byebug'
 
 def install_order(arr)
 
+  vertices_values = {}
+  arr.each do |tuple| 
+    item = tuple[0]
+    dependency = tuple[1]
+    
+    if !vertices_values[item]
+      vertices_values[item] = Vertex.new(item)
+
+    end 
+
+    if !vertices_values[dependency]
+      vertices_values[dependency] = Vertex.new(dependency)
+    end
+    dependency_v = vertices_values[dependency]
+    item_v = vertices_values[item]
+    dependency_to_item = Edge.new(dependency_v, item_v)
+    # item_to_dependency = Edge.new(item_v, dependency_v)
+    dependency_to_item.add_to_edges
+  end 
+
+  sorted = topological_sort(vertices_values.values).map{|el| el.value}
+  total_modules = ((vertices_values.keys.min)..(vertices_values.keys.max)).to_a
+  total_modules.each do |el| 
+    sorted << el if !sorted.include?(el)
+  end 
+  sorted
 end
